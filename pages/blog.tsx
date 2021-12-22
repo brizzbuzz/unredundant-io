e953dc454d400d01d1d317f2b2e5f37d597dc31a
Ryan Brink <rbweb@pm.me>
2021-12-22 17:17:06 -0500

    starting to type

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,51 +1,59 @@
-import type { GetStaticProps, NextPage } from 'next';
-import { Navbar } from '../components/Navbar';
+import type {GetStaticProps, NextPage} from 'next';
+import {Navbar} from '../components/Navbar';
 import path from 'path';
 import fs from 'fs';
 import matter from 'gray-matter';
 
-const Blog: NextPage = ({ posts }) => {
+type PostMetadata = {
+  title: string;
+  date: string; // todo format?
+  description: string;
+  thumbnailUrl: string;
+  tags: string[];
+}
+
+type Post = {
+  slug: string;
+  metadata: PostMetadata
+}
+
+type BlogProps = {
+  posts: Array<Post>
+}
+
+
+const Blog: NextPage<BlogProps> = ({ posts }) => {
   return (
     <div className="container mx-auto min-h-screen">
       <Navbar />
       <div className="min-h-max mx-32 my-10">
         {/*<p className="text-gray-200">Set up blog posts</p>*/}
         {posts.map((post, index) => (
-          <p key={index}>{post.slug}</p>
+          <p key={index}>{post.slug} {post.metadata.date}</p>
         ))}
       </div>
     </div>
   );
 };
 
-type PostMetadata = {
-  title: string;
-  date: string;
-  description: string;
-  thumbnailUrl: string;
-  tags: Array<string>;
-};
-
-type StaticProps = {
-  slug: string;
-  metadata: PostMetadata;
-};
 
 export const getStaticProps: GetStaticProps = async () => {
   const files = fs.readdirSync(path.join('posts'));
-  const posts = files.map((filename) => {
-    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
-    const { data: frontMatter } = matter(markdownWithMeta);
-    return {
-      frontMatter,
-      slug: filename.split('.')[0],
-    };
-  });
+  const posts = files.map((filename) => getPost(filename));
   return {
     props: {
       posts,
     },
   };
 };
+
+function getPost(filename: string): Post {
+  const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
+  const { data: frontMatter } = matter(markdownWithMeta);
+  return {
+    metadata: frontMatter as PostMetadata,
+    slug: filename.split('.')[0],
+  };
+}
 
 export default Blog;
