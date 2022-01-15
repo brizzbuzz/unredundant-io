dc77b0d837bf6d81aa113e46470eb4d9a2b399a6
Ryan Brink <rbweb@pm.me>
2022-01-15 13:22:12 -0500

    pullin pjs

--- pages/api/github.ts
+++ pages/api/github.ts
@@ -1,49 +1,77 @@
 import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
 import { NextApiRequest, NextApiResponse } from 'next';
-import { StrictTypedTypePolicies } from './github-types.generated';
-
-const typePolicies: StrictTypedTypePolicies = {};
 
 const githubClient = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
     Authorization: `bearer ${process.env.GITHUB_TOKEN}`,
   },
-  cache: new InMemoryCache({ typePolicies }),
+  cache: new InMemoryCache(),
 });
 
-type RepositoryInfo = {
+export type RepositoryInfo = {
   repo: string;
   stars: number;
   language: string;
-  release: string;
+  release: string | null;
 };
 
-const handler = async (req: NextApiRequest, res: NextApiResponse<RepositoryInfo>) => {
+const REPOSITORY_INFO = gql`
+  fragment RepositoryInfo on Repository {
+    nameWithOwner
+    stargazerCount
+    latestRelease {
+      name
+      createdAt
+    }
+    primaryLanguage {
+      name
+    }
+  }
+`;
+
+export async function getProjectData() {
   const { data } = await githubClient.query({
     query: gql`
-      query Something {
-        repository(owner: "bkbnio", name: "kompendium") {
-          nameWithOwner
-          stargazerCount
-          latestRelease {
-            name
-            createdAt
-          }
-          primaryLanguage {
-            name
-          }
+      ${REPOSITORY_INFO}
+      query GetRepositories {
+        kompendium: repository(owner: "bkbnio", name: "kompendium") {
+          ...RepositoryInfo
+        }
+        satisfaketion: repository(owner: "unredundant", name: "satisfaketion") {
+          ...RepositoryInfo
+        }
+        sourdoughGradle: repository(owner: "bkbnio", name: "sourdough-gradle") {
+          ...RepositoryInfo
         }
       }
     `,
   });
+  return [
+    {
+      language: data.kompendium.primaryLanguage.name,
+      stars: data.kompendium.stargazerCount,
+      repo: data.kompendium.nameWithOwner,
+      release: data.kompendium.latestRelease.name,
+    },
+    {
+      language: data.satisfaketion.primaryLanguage.name,
+      stars: data.satisfaketion.stargazerCount,
+      repo: data.satisfaketion.nameWithOwner,
+      release: data.satisfaketion.latestRelease?.name ?? null,
+    },
+    {
+      language: data.sourdoughGradle.primaryLanguage.name,
+      stars: data.sourdoughGradle.stargazerCount,
+      repo: data.sourdoughGradle.nameWithOwner,
+      release: data.sourdoughGradle.latestRelease?.name ?? null,
+    },
+  ];
+}
 
-  res.status(200).json({
-    language: data.repository.primaryLanguage.name,
-    stars: data.repository.stargazerCount,
-    repo: data.repository.nameWithOwner,
-    release: data.repository.latestRelease.name,
-  });
+const handler = async (req: NextApiRequest, res: NextApiResponse<Array<RepositoryInfo>>) => {
+  const jsonData = await getProjectData();
+  res.status(200).json(jsonData);
 };
 
 export default handler;
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -2,8 +2,13 @@ import type { NextPage } from 'next';
 import { currentTab } from '../components/Navbar';
 import { useRecoilState } from 'recoil';
 import { PageContainer } from '../components/PageContainer';
+import { getProjectData, RepositoryInfo } from './api/github';
 
-const Projects: NextPage = () => {
+type ProjectProperties = {
+  data: Array<RepositoryInfo>;
+};
+
+const Projects: NextPage<ProjectProperties> = ({ data }) => {
   const [, setCurrent] = useRecoilState(currentTab);
   setCurrent('Projects');
 
@@ -11,9 +16,17 @@ const Projects: NextPage = () => {
     <PageContainer>
       <div className="min-h-max mx-32 my-10">
         <p className="text-offset">Put project details</p>
+        {data.map((project, index) => (
+          <p key={index}>{project.repo}</p>
+        ))}
       </div>
     </PageContainer>
   );
 };
+
+export async function getServerSideProps() {
+  const data = await getProjectData();
+  return { props: { data } };
+}
 
 export default Projects;
