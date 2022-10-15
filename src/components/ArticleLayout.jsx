722e6a05b6ee0bf4002496d74cebe51860035ad4
Ryan Brink <5607577+unredundant@users.noreply.github.com>
2022-10-15 18:12:18 -0400

    spacing

--- src/components/ArticleLayout.jsx
+++ src/components/ArticleLayout.jsx
@@ -61,7 +61,7 @@ export function ArticleLayout({
                 </h1>
                 <time
                   dateTime={meta.date}
-                  className="flex items-center text-base text-zinc-400 dark:text-zinc-500"
+                  className="pt-3 flex items-center text-base text-zinc-400 dark:text-zinc-500"
                 >
                   <span className="h-4 w-0.5 rounded-full bg-zinc-200 dark:bg-zinc-500"/>
                   <span className="ml-3">{formatDate(meta.date)}</span>
--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -45,8 +45,8 @@ export default function ArticlesIndex({ articles }) {
         />
       </Head>
       <SimpleLayout
-        title="Technochad ramblings"
-        intro=""
+        title="Ramblings"
+        intro="You probably don't need to read these"
       >
         <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
