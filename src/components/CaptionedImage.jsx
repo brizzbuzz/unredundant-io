b7c23084b290cbd58452b9465612833f84e8d39d
Ryan Brink <minime@Minime.local>
2022-10-20 09:58:02 -0400

    lookin gewd

--- src/components/CaptionedImage.jsx
+++ src/components/CaptionedImage.jsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 
-export function CaptionedImage({ src }) {
+export function CaptionedImage({src}) {
   return (
-    <div className="mx-auto h-64 w-64">
-      <Image src={src} />
-      <div className="mx-auto border-2 border-blue-300">
-        <p className="mx-auto text-red-400">hi</p>
+    <div className="h-5/6 w-5/6 md:h-1/2 md:w-1/2 mx-auto">
+      <Image src={src}/>
+      <div className="grid grid-cols-1 content-center">
+        <div className="text-gray-500 inline mx-auto font-light text-opacity-90">
+           Madame Security is disappointed
+        </div>
       </div>
+
     </div>
   )
 }
